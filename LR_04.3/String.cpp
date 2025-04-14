226b4d2d9dbd19afc998a6b0f151ed7ffca7a7a2
max-shymanskiy <maxshymanskiy@gmail.com>
2025-04-15 00:27:00 +0300

    Lr.4.3

--- LR_04.3/String.cpp
+++ LR_04.3/String.cpp
@@ -1,13 +1,14 @@
 #include "String.h"
 #include <cstring>
 
-
-String::String(const char* str, int maxLength) : Array(maxLength + 1, 0) {
+String::String(const char* str, int maxLength) : Array(std::min(maxLength, MAX_STRING_SIZE - 1) + 1, 0) {
     setContent(str);
 }
-String::String(int maxLength) : Array(maxLength + 1, 0) {
-	array[0] = 0; // Ініціалізуємо довжину рядка
+
+String::String(int maxLength) : Array(std::min(maxLength, MAX_STRING_SIZE - 1) + 1, 0) {
+    array[0] = 0; 
 }
+
 void String::validatePosition(int pos, bool allowEnd) const {
     int upperBound = allowEnd ? length() : length() - 1;
     if (pos < 0 || pos > upperBound) {
@@ -24,25 +25,23 @@ void String::validateSubstring(const char* substr) const {
 }
 
 void String::setContent(const char* str) {
-    validateSubstring(str); // Перевірка на nullptr
+    validateSubstring(str);
 
     int len = strlen(str);
     if (len > getSize() - 1) len = getSize() - 1;
 
     array[0] = len;
-    for (int i = 0; i < len; ++i) {
-        array[i + 1] = str[i];
-    }
+    memcpy(array + 1, str, len);
 }
 
 void String::setChar(int index, char c) {
-    validatePosition(index); // Перевірка 0 <= index < length()
+    validatePosition(index);
     array[index + 1] = c;
 }
 
 void String::setMaxLength(int newMaxLength) {
-    if (newMaxLength < 1 || newMaxLength > 255) {
-        std::cerr << "Error: Invalid max length" << std::endl;
+    if (newMaxLength < 1 || newMaxLength > MAX_STRING_SIZE - 1) {
+        std::cerr << "Error: Invalid max length. Must be between 1 and " << (MAX_STRING_SIZE - 1) << std::endl;
         exit(1);
     }
 
@@ -50,9 +49,7 @@ void String::setMaxLength(int newMaxLength) {
     int copyLength = std::min(length(), newMaxLength);
 
     newArray[0] = copyLength;
-    for (int i = 0; i < copyLength; ++i) {
-        newArray[i + 1] = array[i + 1];
-    }
+    memcpy(newArray + 1, array + 1, copyLength);
 
     delete[] array;
     array = newArray;
@@ -63,53 +60,102 @@ std::string String::toString() const {
     return std::string((char*)(array + 1), length());
 }
 
-int String::length() const { return array[0]; }
+int String::length() const {
+    return array[0];
+}
 
 int String::find(const char* substr) const {
+    validateSubstring(substr);
+
     int subLen = strlen(substr);
     if (subLen == 0 || subLen > length()) return -1;
 
-    for (int i = 1; i <= length() - subLen + 1; ++i) {
-        bool found = true;
-        for (int j = 0; j < subLen; ++j) {
-            if (array[i + j] != substr[j]) {
-                found = false;
-                break;
-            }
+    for (int i = 0; i <= length() - subLen; ++i) {
+        if (memcmp(array + 1 + i, substr, subLen) == 0) {
+            return i;
         }
-        if (found) return i - 1; // Повертаємо індекс у зручному форматі
     }
     return -1;
 }
 
-void String::remove(int start, int len) {
-    if (start < 0 || len < 0 || start + len > length()) {
-        std::cerr << "Помилка: Невірні параметри видалення" << std::endl;
+String* String::substring(int start, int len) const {
+    validatePosition(start, true);
+
+    if (len < 0 || start + len > length()) {
+        std::cerr << "Error: Invalid substring parameters" << std::endl;
         exit(1);
     }
 
-    for (int i = start + 1; i <= length() - len; ++i) {
-        array[i] = array[i + len];
+    String* result = new String(len);
+    memcpy(result->array + 1, array + start + 1, len);
+    result->array[0] = len;
+
+    return result;
+}
+
+void String::remove(int start, int len) {
+    validatePosition(start, true);
+
+    if (len < 0 || start + len > length()) {
+        std::cerr << "Error: Invalid removal parameters" << std::endl;
+        exit(1);
+    }
+
+    if (start + len < length()) {
+        memmove(array + start + 1, array + start + len + 1, length() - start - len);
     }
     array[0] -= len;
 }
 
+void String::removeSubstring(const char* substr) {
+    validateSubstring(substr);
+
+    int position = find(substr);
+    if (position != -1) {
+        remove(position, strlen(substr));
+    }
+}
+
 void String::insert(int pos, const char* substr) {
+    validatePosition(pos, true);
+    validateSubstring(substr);
+
     int subLen = strlen(substr);
-    if (pos < 0 || pos > length() || length() + subLen > getSize() - 1) {
-        std::cerr << "Помилка: Невірна позиція вставки" << std::endl;
+    if (length() + subLen > getSize() - 1) {
+        std::cerr << "Error: Not enough space for insertion" << std::endl;
         exit(1);
     }
 
-    // Зсув вправо
-    for (int i = length(); i >= pos; --i) {
-        array[i + subLen + 1] = array[i + 1];
+    if (pos < length()) {
+        memmove(array + pos + subLen + 1, array + pos + 1, length() - pos);
     }
-    // Вставка підрядка
-    for (int i = 0; i < subLen; ++i) {
-        array[pos + i + 1] = substr[i];
-    }
+    memcpy(array + pos + 1, substr, subLen);
     array[0] += subLen;
+}
+
+String* String::add(const Array& other) const {
+    const String* s = dynamic_cast<const String*>(&other);
+    if (!s) {
+        std::cerr << "Error: Invalid type" << std::endl;
+        exit(1);
+    }
+
+    int newLength = length() + s->length();
+    if (newLength > MAX_STRING_SIZE - 1) {
+        newLength = MAX_STRING_SIZE - 1;
+        std::cerr << "Warning: String concatenation truncated to maximum length of " << (MAX_STRING_SIZE - 1) << std::endl;
+    }
+
+    String* result = new String(newLength);
+
+    // Copy first string content
+    memcpy(result->array + 1, array + 1, length());
+    // Copy second string content (with possible truncation)
+    int secondLength = std::min(s->length(), MAX_STRING_SIZE - 1 - length());
+    memcpy(result->array + length() + 1, s->array + 1, secondLength);
+
+    result->array[0] = length() + secondLength;
+    return result;
 }
 
 std::ostream& operator<<(std::ostream& os, const String& s) {
@@ -122,27 +168,4 @@ std::istream& operator>>(std::istream& is, String& s) {
     is >> input;
     s.setContent(input.c_str());
     return is;
-}
-
-String* String::add(const Array& other) const {
-    const String* s = dynamic_cast<const String*>(&other);
-    if (!s) {
-        std::cerr << "Помилка: Невірний тип" << std::endl;
-        exit(1);
-    }
-
-    int newLength = length() + s->length();
-    String* result = new String(newLength + 1); // +1 для індексу довжини
-
-    for (int i = 1; i <= length(); ++i) {
-        result->array[i] = array[i];    // Копіюємо символи з поточного рядка
-    }
-    for (int i = 1; i <= s->length(); ++i) {
-        result->array[length() + i] = s->array[i];  // Копіюємо символи з другого рядка
-    }
-
-    // Встановлюємо довжину результуючого рядка
-    result->array[0] = newLength;
-
-    return result;
 }
--- LR_04.3/String.h
+++ LR_04.3/String.h
@@ -4,8 +4,11 @@
 
 class String : public Array {
 private:
+    static const int MAX_STRING_SIZE = 256; // Максимальний розмір рядка (включно з байтом довжини)
+
     void validatePosition(int pos, bool allowEnd = false) const;
     void validateSubstring(const char* substr) const;
+
 public:
     String(int maxLength = 255);
     String(const char* str, int maxLength = 255);
@@ -13,7 +16,11 @@ public:
     int length() const;
     int find(const char* substr) const;
     void remove(int start, int len);
+    void removeSubstring(const char* substr); 
     void insert(int pos, const char* substr);
+
+    String* substring(int start, int len) const;
+
     std::string toString() const;
 
     void setContent(const char* str);
@@ -24,6 +31,4 @@ public:
     friend std::istream& operator>>(std::istream& is, String& s);
 
     virtual String* add(const Array& other) const override;
-
-
 };
